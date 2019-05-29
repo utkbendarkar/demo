8dd7446daa6d382bac561afd5d33916c250dfc39
utkbendarkar <utkbendarkar@yahoo.com>
2019-05-29 17:27:38 +0530

    server changes commited

--- server.js
+++ server.js
@@ -1,2 +1,9 @@
-console.log('Hello Utkarsh...');
-console.log('Hello Hell.>>');
+const express = require('express');
+const bodyParser = require('body-Parser');
+
+const app = express();
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded());
+app.listen(3000,'0.0.0.0',()=>{
+    console.log('Server started on port 3000');
+})
